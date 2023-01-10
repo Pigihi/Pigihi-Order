162107bb967f980c98037f9a40c2e3f2cdeb695a
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2023-01-10 15:51:15 +0530

    Initialize Order microservice repository

---
+++ src/main/java/com/pigihi/PigihiOrderApplication.java
@@ -0,0 +1,13 @@
+package com.pigihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigihiOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigihiOrderApplication.class, args);
+	}
+
+}
